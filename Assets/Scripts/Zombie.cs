5fb5e3e1a80ea1df8b2ea21b6312585b346a0fd6
avelichkov <108222811+avelichkov@users.noreply.github.com>
2024-09-16 22:50:22 -0700

    zombies move forward and you can click to kill them

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -5,21 +5,25 @@ using UnityEngine.UIElements;
 
 public class Zombie : MonoBehaviour
 {
-    public int Health = 5;
+    public int Health = 9;
 
     [HideInInspector]
     public Collider2D feetCol;
 
     [HideInInspector]
     
+    public static float order;
     public Collider2D zone1;
 
     public Stage stage = Stage.NEW;
 
     public enum Stage { NEW = 1,MID = 2, CLOSE = 3,}
 
-    private bool created = false;
+    private bool needsToMove = true;
 
+    public const float MOVE_TIME = 5.0f;
+
+    private float timer = MOVE_TIME;
 
     void Awake()
     {
@@ -31,42 +35,102 @@ public class Zombie : MonoBehaviour
     // Doing this in update so physics update has a change to run
     void Update()
     {
-        if (!created)
+        if (needsToMove)
         {
             if (!IsColliding())
             {
-                print("not in good spot");
                 GoToRandomPos();
             }
             else
             {
-                print("in good spot");
-                GetComponent<SpriteRenderer>().color = new Color(0,255,255,255);
+                if (stage == Stage.CLOSE)
+                {
+                    Vector2 newPos = transform.position;
+                    newPos.y -= 1.5f;
+                    transform.position = newPos;
+                }
+                UpdateZ();
                 GetComponent<Renderer>().enabled = true;
-                created = true;
+                needsToMove = false;
             }
+        }
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            if (stage != Stage.CLOSE)
+            {
+                NextStage();
+            }
+            GetComponent<Renderer>().enabled = false;
+            timer = MOVE_TIME;
+            needsToMove = true;
         }
     }
 
     private void OnMouseDown() {
-        Debug.Log("BRAINZ!");
-        Collider2D colA = GetComponent<Collider2D>();
-        Collider2D colB = GameObject.Find("Zone1").GetComponent<Collider2D>();
-        Debug.Log(IsColliding());
+
+        Debug.Log("Clicked");
+        Destroy(gameObject);
+        // Debug.Log("BRAINZ!");
+        // Collider2D colA = GetComponent<Collider2D>();
+        // Collider2D colB = GameObject.Find("Zone1").GetComponent<Collider2D>();
+        // Debug.Log(IsColliding());
     }
 
     private void GoToRandomPos()
     {
-        float randx = Random.Range(-5.0f,5.0f);
-        float randy = Random.Range(-3.0f,3.0f);
+        float randx = Random.Range(-10.0f,10.0f);
+        float randy = Random.Range(-5.0f,5.0f);
         transform.position = new Vector2(randx,randy);
         //Debug.Log("Going to pos: " + randx + " " + randy);
     }
 
     private bool IsColliding()
     {
+        string zone = null;
+        if (stage == Stage.NEW)
+        {
+           zone = "Zone1";
+        }
+        else if (stage == Stage.MID)
+        {
+            zone = "Zone2";
+        }
+        else if (stage == Stage.CLOSE)
+        {
+           zone = "Zone3"; 
+        }
         Collider2D colA = transform.GetChild(0).GetComponent<Collider2D>();
-        Collider2D colB = GameObject.Find("Zone1").GetComponent<Collider2D>();
+        Collider2D colB = GameObject.Find(zone).GetComponent<Collider2D>();
         return colA.IsTouching(colB);
+    }
+
+    private void NextStage()
+    {
+        if (stage == Stage.NEW)
+        {
+            stage = Stage.MID;
+            transform.localScale = new Vector2(0.1f,0.1f);
+        }
+        else if (stage == Stage.MID)
+        {
+            stage = Stage.CLOSE;
+            transform.localScale = new Vector2(0.5f,0.5f);
+        }
+        else if (stage == Stage.CLOSE)
+        {
+            Debug.Log("no more stages");
+            return;
+        }
+    }
+
+    private void UpdateZ()
+    {
+        float newZ = (int)stage * 10;
+        newZ += order;
+        Vector3 newPos = transform.position;
+        newPos.z = 100 - newZ;
+        transform.position = newPos;
+        order += 0.01f;
     }
 }
--- Assets/Scripts/ZombieSpawner.cs
+++ Assets/Scripts/ZombieSpawner.cs
@@ -10,7 +10,10 @@ public class ZombieSpawner : MonoBehaviour
 
     public GameObject zombie;
     // Start is called before the first frame update
-
+    void Start()
+    {
+        Zombie.order = 0;
+    }
     // Update is called once per frame
     void Update()
     {
